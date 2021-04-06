edee2b9a052201ce793a735cd960710b6de6bb03
L200180194 <alfiantoandy6@gmail.com>
2021-04-06 23:53:11 +0700

    Menambahkan isi theme yang akan digunakan nantinya

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,3 +1,16 @@
 part of 'shared.dart';
 
 const double defaultMargin = 24;
+Color blueColors = "528FFF".toColor();
+Color blueColors2 = "75A6FF".toColor();
+Color greyColor = "4F4F4F".toColor();
+Color greyColor2 = "828282".toColor();
+Color greyColor3 = "A5B2C2".toColor();
+
+TextStyle greyFontStyle = GoogleFonts.inter().copyWith(color: greyColor);
+TextStyle blackFontStyle = GoogleFonts.inter()
+    .copyWith(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w500);
+TextStyle blackFontStyle2 = GoogleFonts.inter()
+    .copyWith(color: Colors.black, fontSize: 26, fontWeight: FontWeight.w500);
+TextStyle blackFontStyle3 = GoogleFonts.inter()
+    .copyWith(color: Colors.black, fontWeight: FontWeight.w500);
