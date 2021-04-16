a2ce55067f93d06cba30e3be9a7d880035a9dddf
L200180194 <alfiantoandy6@gmail.com>
2021-04-16 15:20:05 +0700

    Membbuat alert dialog saat logout

--- lib/pages/gen_coba.dart
+++ lib/pages/gen_coba.dart
@@ -32,6 +32,60 @@ class _GencState extends State<Genc> {
     });
   }
 
+  void onLogOutpressed() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: AlertDialog(
+              backgroundColor: Colors.white70,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8))),
+              title: Center(
+                child: Text(
+                  "Yakin Pingin keluar ?",
+                  style: blackFontStyle3,
+                ),
+              ),
+              content: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  RaisedButton(
+                    onPressed: () {},
+                    color: blueColors,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: blueColors,
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                      child: Text(
+                        "Tidak",
+                        style: blackFontStyle3.copyWith(
+                            fontSize: 11, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  OutlineButton(
+                    borderSide: BorderSide(width: 2, color: blueColors),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    onPressed: () {},
+                    child: Text(
+                      "Ya",
+                      style: blackFontStyle3.copyWith(
+                          fontSize: 11, color: blueColors),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   void _onButtonPressed() {
     showModalBottomSheet(
         shape: RoundedRectangleBorder(
@@ -242,7 +296,7 @@ class _GencState extends State<Genc> {
                                           fontSize: 14),
                                     )),
                                 TextButton.icon(
-                                    onPressed: () => _onButtonPressed(),
+                                    onPressed: () => onLogOutpressed(),
                                     icon: CircleAvatar(
                                       backgroundColor: Colors.red,
                                       radius: 18,
